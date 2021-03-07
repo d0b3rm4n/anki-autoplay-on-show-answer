7025ab5885f4e4e0b48439fbc2ff0fe274ded53c
Reto Zingg <g.d0b3rm4n@gmail.com>
2021-03-07 14:19:54 +0200

    Only play answer_audio if last review for the day

--- __init__.py
+++ __init__.py
@@ -6,11 +6,13 @@ def auto_play_all(card):
     config = mw.addonManager.getConfig(__name__)
     tags = []
 
+    left = int(card.left / 1000)
     if config['play_question_audio']:
         tags += card.question_av_tags()
 
     if config['play_answer_audio']:
-        tags += card.answer_av_tags()
+        if card.queue >= 2 or (card.queue == 1 and left == 1):
+            tags += card.answer_av_tags()
 
     if tags:
         av_player.play_tags(tags)
